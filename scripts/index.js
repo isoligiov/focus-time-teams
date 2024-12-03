e4cb1f9ce2e33963c714bf29af75f04bc9477df1
mac <mac@macs-MacBook-Pro-3.local>
2024-12-03 10:03:48 -0500

    state manamgenet

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,18 @@ const previousState = [];
 (async () => {
   while(true) {
     try {
-      const currentState = []
-      const chatItems = document.querySelectorAll("div[data-shortcut-context='chat-list'] div[role='group'] [data-tid='chat-list-item'] > div[class*='chatListItem']")
+      const currentState = {}
+      const chatItems = document.querySelectorAll("div[data-shortcut-context='chat-list'] div[role='group'] [data-tid='chat-list-item'] > div.chatListItem_mainMedia")
       for(let chatItem of chatItems) {
-        const itemTexts = []
-        for(let child of chatItem.children)
-          itemTexts.push(child.textContent)
-        currentState.push(itemTexts)
+        try {
+          const header = chatItem.querySelector('.chatListItem_mainMedia_header').textContent
+          const timestamp = chatItem.querySelector('.chatListItem_mainMedia_timeStamp').textContent
+          const preview = chatItem.querySelector('.chatListItem_mainMedia_preview').textContent
+          currentState[header] = { timestamp, preview }
+        } catch(err) {}
       }
       console.log(currentState)
+      previousState = currentState
     } catch(err) {
     }
     await sleep(1000)
