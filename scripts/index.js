95cea696189d14a5e4c763b87d9dd6d98be13021
mac <mac@macs-MacBook-Pro-3.local>
2024-12-03 09:56:02 -0500

    fixed issues

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const previousState = [];
   while(true) {
     try {
       const currentState = []
-      const chatItems = document.querySelector("div[data-shortcut-context='chat-list'] div[role='group'] [data-tid='chat-list-item'] > div[class*='chatListItem']")
-      for(let chatItem in chatItems) {
+      const chatItems = document.querySelectorAll("div[data-shortcut-context='chat-list'] div[role='group'] [data-tid='chat-list-item'] > div[class*='chatListItem']")
+      for(let chatItem of chatItems) {
         const itemTexts = []
         for(let child of chatItem.children)
           itemTexts.push(child.textContent)
